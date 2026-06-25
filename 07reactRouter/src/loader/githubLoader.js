0f5d4728c8dbd90297bced9851d2fd8e30c99229
Shubhdeep Sharma <shubhdeepsharma213@gmail.com>
2026-06-25 15:47:47 +0530

    remove unused projects

--- 07reactRouter/src/loader/githubLoader.js
+++
@@ -1,6 +0,0 @@
-const githubInfoLoader = async () => {
-    const response = await fetch("https://api.github.com/users/shubhdeep123")
-    return response.json();
-}
-
-export default githubInfoLoader;
